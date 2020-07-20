e4049db3ea178b7baeb4702ea17bfd8402e40dd0
Bonnie <hz_liuzb@163.com>
2020-07-20 17:18:10 +0800

    修改了XXLPlane这个文件的注释

--- shoot/src/com/zzxx/shoot/XXLPlane.java
+++ shoot/src/com/zzxx/shoot/XXLPlane.java
@@ -2,6 +2,9 @@ package com.zzxx.shoot;
 
 import java.awt.image.BufferedImage;
 
+/*
+这是超大号飞机,还没完成 - 第二次修改
+ */
 public class XXLPlane extends FlyingObject implements Enemy, Award {
     protected XXLPlane(int x, int y, BufferedImage img, int life) {
         super(x, y, img, life);
